c1c8ee6add62ea39241a455973424c8daa506439
Besita Augustin <besita2000@gmail.com>
2025-06-16 17:24:54 +0530

    Time: 3 ms (79.89%) | Memory: 44.9 MB (51.68%) - LeetSync

---
+++ Arrays/13-roman-to-integer/roman-to-integer.java
@@ -0,0 +1,65 @@
+class Solution {
+    public int usingHashMap(String s){
+        Map<Character,Integer> map=new HashMap<>();
+        map.put('I',1);
+        map.put('V',5);
+        map.put('X',10);
+        map.put('L',50);
+        map.put('C',100);
+        map.put('D',500);
+        map.put('M',1000);
+        int n=s.length();
+        int ans=0;
+        for(int i=n-1;i>=0;i--){
+            char ch=s.charAt(i);
+            if((ch=='V' || ch=='X') && (i>0 && s.charAt(i-1)=='I')){
+                ans+=map.get(ch)-1;
+                i--;
+            }
+            else if((ch=='L' || ch=='C') && (i>0 && s.charAt(i-1)=='X')){
+                ans+=map.get(ch)-10;
+                i--;
+            }
+            else if((ch=='D' || ch=='M') && (i>0 && s.charAt(i-1)=='C')){
+                ans+=map.get(ch)-100;
+                i--;
+            }
+            else{
+                ans+=map.get(ch);
+            }
+        }
+        return ans;
+    }
+    public int value(char c){
+        switch(c){
+            case('I') : return 1;
+            case('V') : return 5;
+            case('X') : return 10;
+            case('L') : return 50;
+            case('C') : return 100;
+            case('D') : return 500;
+            case('M') : return 1000;
+            default : return 0;
+        }
+    }
+    public int usingSwitch(String s){
+        int n=s.length();
+        int ans=0;
+        for(int i=n-1;i>=0;i--){
+            char ch=s.charAt(i);
+            if(i>0 && value(ch)>value(s.charAt(i-1))){
+                ans+=value(ch)-value(s.charAt(i-1));
+                i--;
+            }
+            else
+                ans+=value(ch);
+        }
+        return ans;
+    }
+    public int romanToInt(String s) {
+        //TC:O(n) SC:O(n)
+        //return usingHashMap(s);
+        //TC:O(n) SC:O(1)
+        return usingSwitch(s);
+    }
+}

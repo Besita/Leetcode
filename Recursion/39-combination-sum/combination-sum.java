3b99ef22a746c3798541f91d8cb77576b1ed0c1f
Besita Augustin <besita2000@gmail.com>
2025-06-19 22:37:25 +0530

    Time: 2 ms (83.69%) | Memory: 44.8 MB (46.72%) - LeetSync

---
+++ Recursion/39-combination-sum/combination-sum.java
@@ -0,0 +1,21 @@
+class Solution {
+    public void backtracking(int[] candidates,int n,int curr,int target,List<Integer> temp,List<List<Integer>> ans){
+        if(target<=0 || curr>=n){
+            if(target==0)
+                ans.add(new ArrayList(temp));
+            return;
+        }
+
+        temp.add(candidates[curr]);
+        backtracking(candidates,n,curr,target-candidates[curr],temp,ans);
+        temp.remove(temp.size()-1);
+        backtracking(candidates,n,curr+1,target,temp,ans);
+    }
+    public List<List<Integer>> combinationSum(int[] candidates, int target) {
+        List<List<Integer>> ans=new ArrayList<List<Integer>>();
+        List<Integer> temp=new ArrayList<>();
+        int n=candidates.length;
+        backtracking(candidates,n,0,target,temp,ans);
+        return ans;
+    }
+}

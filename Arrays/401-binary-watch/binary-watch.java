f44ef931943eaf286e38cc46cb11f37cd4ab0910
Besita Augustin <besita2000@gmail.com>
2025-06-19 17:15:13 +0530

    Time: 5 ms (81.11%) | Memory: 42.7 MB (17.93%) - LeetSync

--- Arrays/401-binary-watch/binary-watch.java
+++ Arrays/401-binary-watch/binary-watch.java
@@ -16,7 +16,36 @@ class Solution {
         }
         return ans; 
     }
+    public void backtracking(int turnedon,int curr,int hrs,int min,List<String> ans,int[] arr){
+        if(turnedon==0){
+            if(hrs<12 && min<60){
+                if(min<10)
+                        ans.add(hrs+":0"+min);
+                    else
+                        ans.add(hrs+":"+min);
+            }
+            return;
+        }
+        for(int i=curr;i<arr.length;i++){
+            if(i<4)  hrs+=arr[i];
+            else     min+=arr[i];
+
+            backtracking(turnedon-1,i+1,hrs,min,ans,arr);
+
+            if(i<4)  hrs-=arr[i];
+            else     min-=arr[i];
+        }
+    }
     public List<String> readBinaryWatch(int turnedOn) {
-          return bruteforce(turnedOn);//TC:O(12*60) SC:O(12*60)
+        //return bruteforce(turnedOn);//TC:O(12*60) SC:O(12*60)
+
+        if(turnedOn>10)
+            return new ArrayList<>();
+
+
+        List<String> ans=new ArrayList<>();
+        int[] arr={1,2,4,8,1,2,4,8,16,32};
+        backtracking(turnedOn,0,0,0,ans,arr);
+        return ans;
     }
 }

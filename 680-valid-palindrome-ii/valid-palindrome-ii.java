4a4a812dbd42ea8cf4d33cb1db3312ba6a376a0a
Besita Augustin <besita2000@gmail.com>
2025-06-27 00:46:17 +0530

    Time: 20 ms (5.83%) | Memory: 45.8 MB (15.90%) - LeetSync

--- 680-valid-palindrome-ii/valid-palindrome-ii.java
+++ 680-valid-palindrome-ii/valid-palindrome-ii.java
@@ -18,14 +18,10 @@ class Solution {
         int l=0,r=n-1;
         while(l<r){
             if(s.charAt(l)!=s.charAt(r)){ 
-                if(ispalindrome(s.substring(0,l)+s.substring(l+1)) || ispalindrome(s.substring(0,r)+s.substring(r+1)) )
-                    return true;
-                return false;
-            }
-            else{ 
-                l++;
-                r--;
-            }
+                return (ispalindrome(s.substring(0,l)+s.substring(l+1)) || ispalindrome(s.substring(0,r)+s.substring(r+1)) );
+            }    
+            l++;
+            r--;
         }
         return true;
     }

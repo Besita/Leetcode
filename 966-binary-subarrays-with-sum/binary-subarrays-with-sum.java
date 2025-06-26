4ee43fb630dd9e5f08486724a9558c5139ff7b8d
Besita Augustin <besita2000@gmail.com>
2025-06-26 23:05:23 +0530

    Time: 1 ms (100.00%) | Memory: 47.9 MB (54.10%) - LeetSync

---
+++ 966-binary-subarrays-with-sum/binary-subarrays-with-sum.java
@@ -0,0 +1,25 @@
+
+class Solution {
+    int slidingwindow(int[] nums,int goal){
+        int left=0;
+        int ans=0;
+        int sum=0;
+        if(goal<0)
+            return 0;
+        for(int right=0;right<nums.length;right++){
+            sum+=nums[right];
+            
+            while(sum>goal){
+                sum-=nums[left];
+                left++;
+            }
+
+            ans+=right-left+1;
+        }
+        return ans;
+    }
+    public int numSubarraysWithSum(int[] nums, int goal) {
+        
+        return slidingwindow(nums,goal)-slidingwindow(nums,goal-1);
+    }
+}

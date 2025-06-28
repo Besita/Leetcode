ea1b451847c8c175320ceb86777e66d9ac599737
Besita Augustin <besita2000@gmail.com>
2025-06-28 23:35:49 +0530

    Time: 14 ms (44.23%) | Memory: 55.6 MB (21.25%) - LeetSync

--- 1370-count-number-of-nice-subarrays/count-number-of-nice-subarrays.java
+++ 1370-count-number-of-nice-subarrays/count-number-of-nice-subarrays.java
@@ -38,8 +38,27 @@ class Solution {
         }
         return ans; 
     }
+    public int twopointer(int[] nums,int k){
+        int left=0;
+        int odd=0;
+        int ans=0;
+        for(int right=0;right<nums.length;right++){
+            if(nums[right]%2==1)
+                odd++;
+
+            while(odd>k){
+                if(nums[left]%2==1)
+                    odd--;
+                left++;
+            }
+
+            ans+=right-left+1;
+        }
+        return ans;
+    }
     public int numberOfSubarrays(int[] nums, int k) {
        //return optimized(nums,k);
-       return threepointer(nums,k);
+       //return threepointer(nums,k);
+       return twopointer(nums,k)-twopointer(nums,k-1);
     }
 }

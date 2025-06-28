cd841b9fc8d118db4b7627c529b0b423c36dcda3
Besita Augustin <besita2000@gmail.com>
2025-06-29 00:25:27 +0530

    Time: 51 ms (31.09%) | Memory: 47.7 MB (43.19%) - LeetSync

--- 1034-subarrays-with-k-different-integers/subarrays-with-k-different-integers.java
+++ 1034-subarrays-with-k-different-integers/subarrays-with-k-different-integers.java
@@ -1,5 +1,5 @@
 class Solution {
-    public int subarraysWithKDistinct(int[] nums, int k) {
+    int using3pointer(int[] nums,int k){
         HashMap<Integer,Integer> map=new HashMap<>();
         int ans=0;
         int left=0;
@@ -23,5 +23,28 @@ class Solution {
                 ans+=mid-left+1;
         }
         return ans;
+    }
+    int using2pointer(int[] nums,int k){
+        int left=0;
+        int count=0;
+        int n=nums.length;
+        Map<Integer,Integer> map=new HashMap<>();
+        for(int right=0;right<nums.length;right++){
+            map.put(nums[right],map.getOrDefault(nums[right],0)+1);
+
+            while(map.size()>k){
+                map.put(nums[left],map.getOrDefault(nums[left],0)-1);
+                if(map.get(nums[left])==0)
+                    map.remove(nums[left]);
+                left++;
+            }
+
+            count+=right-left+1;
+        }
+        return count;
+    }
+    public int subarraysWithKDistinct(int[] nums, int k) {
+        //return using3pointer(nums,k);
+        return using2pointer(nums,k)-using2pointer(nums,k-1);
     }
 }

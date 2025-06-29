a365c7a32ef3ab5946bb77b642c02f10625aefa7
Besita Augustin <besita2000@gmail.com>
2025-06-30 00:11:31 +0530

    Time: 4 ms (60.11%) | Memory: 50.6 MB (16.08%) - LeetSync

---
+++ 229-majority-element-ii/majority-element-ii.java
@@ -0,0 +1,89 @@
+class Solution {
+    public List<Integer> usingHashmap(int[] nums){
+        List<Integer> ans=new ArrayList<>();
+        Map<Integer,Integer> map=new HashMap<>();
+        int n=nums.length;
+        for(int i=0;i<n;i++){
+            map.put(nums[i],map.getOrDefault(nums[i],0)+1);
+        }
+
+        int thresh=n/3;
+        for(Map.Entry<Integer,Integer> i:map.entrySet()){
+            int val=i.getKey();
+            int count=i.getValue();
+            if(count>thresh)
+                ans.add(val);
+        }
+        return ans;
+    }
+    public List<Integer> usingVoterAlgo(int[] nums){
+        int n=nums.length;
+        int thresh=n/3;
+        List<Integer> ans=new ArrayList<>();
+        int count1=0;
+        int count2=0;
+        int candidate1=0;
+        int candidate2=0;
+        for(int i=0;i<n;i++){
+            if(count1==0 && candidate2!=nums[i]){
+                candidate1=nums[i];
+                count1=1;
+            }
+            else if(count2==0 && candidate1!=nums[i]){
+                candidate2=nums[i];
+                count2=1;
+            }
+            else if(candidate1==nums[i]){ 
+                count1++;
+            }
+            else if(candidate2==nums[i]){ 
+                count2++;
+            }
+            else{
+                count1--;
+                count2--;
+            }
+        }
+        count1=0;
+        count2=0;
+        for(int i=0;i<n;i++){
+            if(nums[i]==candidate1)
+                count1++;
+            else if(nums[i]==candidate2)
+                count2++;
+        }
+
+        if(count1>thresh){ 
+            ans.add(candidate1);
+        }
+        if(count2>thresh){ 
+            ans.add(candidate2);
+        }
+        return ans;
+    }
+    List<Integer> usingsorting(int[] nums){
+        int n=nums.length;
+        int freq=n/3+1;
+        Arrays.sort(nums);
+        int count=0;
+        List<Integer> ans=new ArrayList<>();
+        int currval;
+        //int i=0;
+        for(int i=0;i<n;){           
+            currval=nums[i];
+            while(i<n && nums[i]==currval){
+                count++;
+                i++;
+            }
+            if(count>=freq)
+                ans.add(currval);
+            count=0;           
+        }
+        return ans;
+    }
+    public List<Integer> majorityElement(int[] nums) {
+       //return usingHashmap(nums);
+        return usingsorting(nums);
+        //return usingVoterAlgo(nums);//using Moore's voter algorithm
+    }
+}

59c10e9ca65ecd7cae17985ea548681f54f444ec
Besita Augustin <besita2000@gmail.com>
2025-06-30 00:32:05 +0530

    Time: 3 ms (54.51%) | Memory: 53.1 MB (59.69%) - LeetSync

---
+++ 169-majority-element/majority-element.java
@@ -0,0 +1,28 @@
+class Solution {
+    int usingsort(int[] nums){
+        int n=nums.length;
+        Arrays.sort(nums);
+        return nums[n/2];
+    }
+    public int majorityElement(int[] nums) {
+        //using sort
+        return usingsort(nums);
+
+       /* int n=nums.length;
+        int count=0;
+        int candidate=0;
+        for(int i=0;i<n;i++){
+            if(count==0){
+                candidate=nums[i];
+                count++;
+            }
+            else{
+                if(candidate==nums[i])
+                    count++;
+                else
+                    count--;
+            }
+        }
+        return candidate;*/
+    }
+}

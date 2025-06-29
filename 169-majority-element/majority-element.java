22d2c494ad7daec54f9e2f7ad2d7e958100d54b4
Besita Augustin <besita2000@gmail.com>
2025-06-30 00:38:23 +0530

    Time: 6 ms (39.61%) | Memory: 53.6 MB (8.41%) - LeetSync

--- 169-majority-element/majority-element.java
+++ 169-majority-element/majority-element.java
@@ -4,9 +4,27 @@ class Solution {
         Arrays.sort(nums);
         return nums[n/2];
     }
+    int usingvoteralgo(int[] nums){
+        Arrays.sort(nums);
+        int n=nums.length;
+        int count1=0;
+        int candidate1=0;
+        for(int i=0;i<n;i++){
+            if(count1==0){
+                candidate1=nums[i];
+                count1++;
+            }
+            else if(candidate1==nums[i])
+                count1++;
+            else
+                count1--;
+        }
+        return candidate1;
+    }
     public int majorityElement(int[] nums) {
         //using sort
-        return usingsort(nums);
+        //return usingsort(nums);
+        return usingvoteralgo(nums);
 
        /* int n=nums.length;
         int count=0;

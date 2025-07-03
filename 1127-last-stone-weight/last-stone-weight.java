fdaba61908c3a81895a89f34cdc7c062f9d84cb0
Besita Augustin <besita2000@gmail.com>
2025-07-04 00:04:19 +0530

    Time: 1 ms (98.14%) | Memory: 41.6 MB (13.47%) - LeetSync

--- 1127-last-stone-weight/last-stone-weight.java
+++ 1127-last-stone-weight/last-stone-weight.java
@@ -1,5 +1,5 @@
 class Solution {
-    public int lastStoneWeight(int[] stones) {
+    public int usingHeap(int[] stones){
         PriorityQueue<Integer> pq=new PriorityQueue<>(Collections.reverseOrder());
         for(int i:stones)
             pq.add(i);
@@ -15,5 +15,20 @@ class Solution {
             return pq.poll();
 
         return 0;
+    }
+    public int usingArray(int[] stones){
+        int n=stones.length;
+        Arrays.sort(stones);        
+        for(int i=n-1;i>0;i--){
+            stones[i-1]=stones[i]-stones[i-1];
+            Arrays.sort(stones);
+        }
+        return stones[0];
+    }
+    public int lastStoneWeight(int[] stones) {
+
+        //return usingHeap(stones);//TC:(n logn) SC:O(n)
+        return usingArray(stones);//TC:O(n logn) SC:O(1)
+        
     }
 }

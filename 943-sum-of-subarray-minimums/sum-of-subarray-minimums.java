9477d8b20c93296c8bcb10d49514855203e62824
Besita Augustin <besita2000@gmail.com>
2025-07-05 22:30:39 +0530

    Time: 78 ms (8.25%) | Memory: 50.2 MB (78.82%) - LeetSync

---
+++ 943-sum-of-subarray-minimums/sum-of-subarray-minimums.java
@@ -0,0 +1,50 @@
+class Solution {
+    int[] MinElementLeft(
+        int[] arr){
+        int n=arr.length;
+        Stack<Integer> st=new Stack<>();
+        int[] ans=new int[n];
+        Arrays.fill(ans,-1);
+        for(int i=0;i<n;i++){
+            while(!st.isEmpty() && arr[st.peek()]>=arr[i])
+                st.pop();
+            if(!st.isEmpty())
+                ans[i]=st.peek();
+
+            st.push(i);
+            //System.out.print(" "+ans[i]);
+        }
+        return ans;
+    }
+    int[] MinElementRight(int[] arr){
+        int n=arr.length;
+        Stack<Integer> st=new Stack<>();
+        int[] ans=new int[n];
+        Arrays.fill(ans,n);
+        //System.out.println();
+        for(int i=n-1;i>=0;i--){
+            while(!st.isEmpty() && arr[st.peek()]>arr[i])
+                st.pop();
+            if(!st.isEmpty())
+                ans[i]=st.peek();
+
+            st.push(i);
+           // System.out.print(" "+ans[i]);
+        } 
+        return ans;
+    }
+    public int sumSubarrayMins(int[] arr) {
+        int n=arr.length;
+        int[] leftmin=MinElementLeft(arr);
+        int[] rightmin=MinElementRight(arr);
+        long ans=0;
+        int mod=(int)1e9+7;
+        for(int i=0;i<n;i++){
+            int left=(i-leftmin[i])%mod;
+            int right=(rightmin[i]-i)%mod;
+            ans+=(long)(left*right)%mod * arr[i]%mod;
+            ans%=mod;
+        }
+        return (int)ans;
+    }
+}

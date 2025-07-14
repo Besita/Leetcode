8db641ba45c222f2dc1d22ee2a281d6415a260d2
Besita Augustin <besita2000@gmail.com>
2025-07-14 12:58:45 +0530

    Time: 25 ms (96.17%) | Memory: 51.4 MB (70.41%) - LeetSync

--- 15-3sum/3sum.java
+++ 15-3sum/3sum.java
@@ -29,8 +29,43 @@ class Solution {
         }
         return ans;
     }
+     public List<List<Integer>> handleduplicatewithoutset(int[] nums) {
+        Arrays.sort(nums);
+        int n=nums.length;
+        List<List<Integer>> ans=new ArrayList<>();
+        int curr=0;
+        int left;
+        int right;
+        for(curr=0;curr<n-2;curr++){ 
+            //avoid duplicates
+            if(nums[curr]>0 || (curr-1>=0 && nums[curr]==nums[curr-1]))
+                continue;
+
+            left=curr+1;
+            right=n-1;
+            while(left<right){ 
+                int sum=nums[curr]+nums[left]+nums[right];
+                if(sum==0){
+                    ans.add(Arrays.asList(nums[curr],nums[left],nums[right]));
+                    int currleft=nums[left];
+                    int currright=nums[right];
+                    while(left<right && nums[left+1]==currleft) left++;
+                    while(left<right && nums[right-1]==currright)   right--;
+                    left++;
+                    right--;
+                }
+                else if(sum<0)
+                    left++;
+                else
+                    right--;
+            }
+        }
+        
+        return ans;
+    }
     public List<List<Integer>> threeSum(int[] nums) {
-        return usingSet(nums);
+        //return usingSet(nums); //TC:O(n*n) SC:O(n)
+        return handleduplicatewithoutset(nums); //TC:O(n*n) SC:O(1)
         
     }
 }            

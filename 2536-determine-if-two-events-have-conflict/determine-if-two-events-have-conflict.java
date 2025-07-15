3452d33051635070e42239f05f469b4e8783835c
Besita Augustin <besita2000@gmail.com>
2025-07-15 20:41:11 +0530

    Time: 0 ms (100.00%) | Memory: 41.1 MB (73.31%) - LeetSync

--- 2536-determine-if-two-events-have-conflict/determine-if-two-events-have-conflict.java
+++ 2536-determine-if-two-events-have-conflict/determine-if-two-events-have-conflict.java
@@ -1,7 +1,6 @@
 class Solution {
-    public boolean haveConflict(String[] event1, String[] event2) {
-        int n=event1.length;
-
+    public boolean usingInt(String[] event1, String[] event2){
+         int n=event1.length;        
         int[] meet1=new int[n];
         for(int i=0;i<n;i++){ 
             int hr=Integer.parseInt(event1[i].substring(0,2));
@@ -18,9 +17,23 @@ class Solution {
             meet2[i]=tottime;
         }
 
-        System.out.println(meet1[1]+" "+meet2[0]);
+        //System.out.println(meet1[1]+" "+meet2[0]);
         if((meet1[0]<=meet2[0] && meet1[1]>=meet2[0]) || (meet1[0]>meet2[0] && meet2[1]>=meet1[0]))
             return true;
         return false;
+    }
+     public boolean usingString(String[] event1, String[] event2){
+        String start1=event1[0];
+        String end1=event1[1];
+        String start2=event2[0];
+        String end2=event2[1];
+        
+        if((end1.compareTo(start2)<0) || (end2.compareTo(start1)<0))
+            return false;
+        return true;
+    }
+    public boolean haveConflict(String[] event1, String[] event2) {
+       //return usingInt(event1,event2);
+       return usingString(event1,event2);
     }
 }

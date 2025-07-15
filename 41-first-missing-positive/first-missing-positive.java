ad190a774c3fc26d89ef9777a73962b41dc011be
Besita Augustin <besita2000@gmail.com>
2025-07-15 13:27:48 +0530

    Time: 619 ms (5.87%) | Memory: 56.1 MB (96.50%) - LeetSync

--- 41-first-missing-positive/first-missing-positive.java
+++ 41-first-missing-positive/first-missing-positive.java
@@ -1,7 +1,11 @@
 class Solution {
-    public int usingArray(int[] nums){
+    public int usingNegation(int[] nums){
+        //loop and check whether 1 is there, if no return 1.
+        //use negation technique for numbers from [1-n-1]. 
+        //use idx 0 to track n
+        //if all these numbers are present the missing should be n+1
+        
         int n=nums.length;
-
         int onepresent=0;
         for(int i=0;i<n;i++){
             if(nums[i]==1)
@@ -13,7 +17,6 @@ class Solution {
             return 1;
 
         for(int i=0;i<n;i++){ 
-            //System.out.println(" "+nums[i]);
             if(Math.abs(nums[i])==n)
                nums[0]=-Math.abs(nums[0]);
             else
@@ -28,9 +31,29 @@ class Solution {
             return n;
         return n+1;
     }
+    public void swap(int[] arr,int a,int b){
+        int c=arr[a];
+        arr[a]=arr[b];
+        arr[b]=c;
+    }
+    public int optimized(int[] nums){
+        int n=nums.length;
+        for(int i=0;i<n;i++){
+            while(nums[i]>0 && nums[i]<=n && nums[i]!=nums[nums[i]-1])
+                swap(nums,i,nums[i]-1);
+        }
+        for(int i=0;i<n;i++)
+            System.out.print(" "+nums[i]);
+
+        for(int i=0;i<n;i++){
+            if(nums[i]!=i+1)
+                return i+1;
+        }
+        return n+1;
+    }
     public int firstMissingPositive(int[] nums) {
-        return usingArray(nums);//TC:O(n) SC:O(n) 
-        //return optimized(nums);//TC:O(n) SC:O(1)
+        //return usingNegation(nums);//TC:O(n) SC:O(n) 
+        return optimized(nums);//TC:O(n) SC:O(1)
         
     }
 }

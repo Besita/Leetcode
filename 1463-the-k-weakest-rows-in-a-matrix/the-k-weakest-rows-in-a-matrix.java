a0d6457914fddf8c04bf843c0e323b1c6a43c0eb
Besita Augustin <besita2000@gmail.com>
2025-07-15 17:38:35 +0530

    Time: 2 ms (70.04%) | Memory: 44.6 MB (94.75%) - LeetSync

--- 1463-the-k-weakest-rows-in-a-matrix/the-k-weakest-rows-in-a-matrix.java
+++ 1463-the-k-weakest-rows-in-a-matrix/the-k-weakest-rows-in-a-matrix.java
@@ -1,7 +1,9 @@
 class Solution {
-    public int[] kWeakestRows(int[][] mat, int k) {
-        //priorityQueue<Pair<Integer,Integer>> pq=new PriorityQueue<>(a->b -> (a[0] == b[0]) ? a[1] - b[1] : a[0] - b[0]);
-        
+
+    public int[] usingArrays(int[][] mat,int k){
+        //we no need to use priorityqueue since sorting is needed onlt 1 time.
+        //we can further optimise by using binary search for counting soldiers
+        // since soldiers are standing before civilians
         int m=mat.length;
         int n=mat[0].length;
         int[][] pq=new int[m][2];
@@ -21,5 +23,48 @@ class Solution {
             ans[i]=pq[i][1];
         }
         return ans;
+    }
+     public int[] usingBinarysearch(int[][] mat,int k){
+        int m=mat.length;
+        int n=mat[0].length;
+        int[][] pq=new int[m][2];
+        for(int i=0;i<m;i++){
+            int rowsum=0;
+            int left=0;
+            int right=n-1;
+            if(mat[i][0]==0) {
+                pq[i][0]=0;
+                pq[i][1]=i;
+            }
+            else  if(mat[i][right]==1){
+                pq[i][0]=n;
+                pq[i][1]=i;
+            }
+            else{
+                while(left<=right){
+                    int mid=(left+right)/2;
+                    if(mat[i][mid]==1 && mat[i][mid+1]==0){
+                        rowsum=mid+1;
+                        break;
+                    }
+                    else if(mat[i][mid]==0)
+                        right=mid-1;
+                    else
+                        left=mid+1;
+                }
+                pq[i][0]=rowsum;
+                pq[i][1]=i;    
+            }  
+        }
+        Arrays.sort(pq,(a,b)->(a[0]==b[0])?a[1]-b[1]:a[0]-b[0]);
+        int[] ans=new int[k];
+        for(int i=0;i<k;i++){
+            ans[i]=pq[i][1];
+        }
+        return ans;
+    }
+    public int[] kWeakestRows(int[][] mat, int k) {
+        //return usingArrays(mat,k); //TC:O(n*m(calculating strength) + nlogn(sort)) SC:O(n*2) //3ms
+        return usingBinarysearch(mat,k); ////TC:O(n*logm(calculating strength) + nlogn(sort)) SC:O(n*2)
     }
 }

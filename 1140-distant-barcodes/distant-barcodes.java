4b5f94c36f483ea1deee2c3d85b2356231e45654
Besita Augustin <besita2000@gmail.com>
2025-07-18 13:16:30 +0530

    Time: 26 ms (77.87%) | Memory: 46.7 MB (24.59%) - LeetSync

---
+++ 1140-distant-barcodes/distant-barcodes.java
@@ -0,0 +1,28 @@
+class Solution {
+    public int[] rearrangeBarcodes(int[] barcodes) {
+        int n=barcodes.length;
+        PriorityQueue<int[]> pq=new PriorityQueue<>((a,b)->(b[0]-a[0]));
+        HashMap<Integer,Integer> map=new HashMap<>();
+        for(int i:barcodes){
+            map.put(i,map.getOrDefault(i,0)+1);
+        }
+        
+        for(int i:map.keySet()){ 
+            pq.add(new int[]{map.get(i),i});
+           // System.out.println(map.get(i)+" "+i);
+        }
+        
+        int[] ans=new int[n];
+        int i=0;
+        while(!pq.isEmpty()){
+            int[] curr=pq.poll();
+            int count=curr[0];
+            while(count-- >0){
+                if(i>=n) i=1;
+                ans[i]=curr[1];
+                i+=2;
+            }
+        }
+        return ans;
+    }
+}

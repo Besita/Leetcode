319a5c062e9910f4b923aaec996df3bc87a14a17
Besita Augustin <besita2000@gmail.com>
2025-07-20 15:39:06 +0530

    Time: 30 ms (62.72%) | Memory: 51.2 MB (64.73%) - LeetSync

--- 1014-k-closest-points-to-origin/k-closest-points-to-origin.java
+++ 1014-k-closest-points-to-origin/k-closest-points-to-origin.java
@@ -1,5 +1,6 @@
+
 class Solution {
-    public int[][] kClosest(int[][] points, int k) {
+    public int[][] usingpriorityQueue(int[][] points, int k) {
         int n=points.length;
         PriorityQueue<int[]> pq=new PriorityQueue<>((a,b)->b[0]-a[0]);
         for(int i=0;i<n;i++){
@@ -18,5 +19,24 @@ class Solution {
             i++;
         }
         return ans;
+    }
+    public int[][] usingsorting(int[][] points, int k) {
+        int n=points.length;
+        int[][] arr=new int[n][2];
+        for(int i=0;i<n;i++){
+           int dist=(points[i][0]*points[i][0]) + (points[i][1]*points[i][1]);
+           arr[i]=new int[]{dist,i};
+        }
+        Arrays.sort(arr,(a,b)->a[0]-b[0]);
+        int[][] ans=new int[k][2];
+        for(int i=0;i<k;i++){        
+            ans[i][0]=points[arr[i][1]][0];
+            ans[i][1]=points[arr[i][1]][1];
+        }
+        return ans;
+    }
+    public int[][] kClosest(int[][] points, int k) {
+        //return usingpriorityQueue(points,k);
+        return usingsorting(points,k);
     }
 }

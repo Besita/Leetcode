2cfe057b21774826ff8cbd79d39c4a8e33c1a42d
Besita Augustin <besita2000@gmail.com>
2025-07-21 21:40:26 +0530

    Time: 46 ms (16.17%) | Memory: 46.1 MB (5.38%) - LeetSync

--- 23-merge-k-sorted-lists/merge-k-sorted-lists.java
+++ 23-merge-k-sorted-lists/merge-k-sorted-lists.java
@@ -39,15 +39,15 @@ class Solution {
     }
     public ListNode usingmergesort(ListNode[] lists){
         int k=lists.length;
-        if(k==0)    return null;
-        if(k==1)    return lists[0];
-    
+       // if(k==0)    return null;
+        //if(k==1)    return lists[0];
+
         for(int i=1;i<k;i++)
             lists[i]=mergesort(lists[i-1],lists[i]);
 
         return lists[k-1];
     }
-    public ListNode usingarrays(ListNode[] lists){
+    public ListNode usingsorting(ListNode[] lists){
         int n=lists.length;
         List<Integer> arr=new ArrayList<>();
         for(int i=0;i<n;i++){
@@ -66,9 +66,47 @@ class Solution {
         }
         return res.next;
     }
+    public ListNode usingArrays(ListNode[] lists){
+        int minval=Integer.MAX_VALUE;
+        int maxval=Integer.MIN_VALUE;
+        int n=lists.length;
+        for(int i=0;i<n;i++){
+            ListNode curr=lists[i];
+            while(curr!=null){
+                minval=Math.min(minval,curr.val);
+                maxval=Math.max(maxval,curr.val);
+                System.out.println(maxval+" "+minval);
+                curr=curr.next;
+            }
+        }
+        int bucketlen=maxval-minval+1;
+        System.out.println(maxval+" "+minval+" "+bucketlen);
+        int[] bucket=new int[bucketlen];
+        for(int i=0;i<n;i++){
+            ListNode curr=lists[i];
+            while(curr!=null){
+                bucket[curr.val-minval]++;
+                curr=curr.next;
+            }
+        }   
+        ListNode ans=new ListNode(-1);
+        ListNode res=ans;
+        for(int i=0;i<bucketlen;i++){
+            while(bucket[i]-- > 0){ 
+                res.next=new ListNode(i+minval);
+                res=res.next;
+            }
+        }
+        return ans.next;
+    }
     public ListNode mergeKLists(ListNode[] lists) {
-        ///return usingmergesort(lists);
-        return usingarrays(lists);
+        int k=lists.length;
+        if(k==0)    return null;
+        if(k==1)    return lists[0];
+
+        ///return usingmergesort(lists); //memeory limit exceeded
+        //return usingsorting(lists);
+        return usingArrays(lists);
         
     }
 }

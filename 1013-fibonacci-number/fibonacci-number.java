7d886863151a11f46b5aec602b1101116df1e275
Besita Augustin <besita2000@gmail.com>
2025-08-08 22:42:26 +0530

    Time: 0 ms (100.00%) | Memory: 40.9 MB (6.59%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.java
+++ 1013-fibonacci-number/fibonacci-number.java
@@ -1,9 +1,24 @@
 class Solution {
-    public int fib(int n) {
+    public int recursion(int n){
         if(n==0)
             return 0;
         if(n==1)
             return 1;
-        return (fib(n-1)+fib(n-2));
+        return (fib(n-1)+fib(n-2)); 
+    }
+    public int topdown(int n){
+        int[] arr=new int[n+1];
+        if(n<=1)
+            return n;
+        arr[0]=0;
+        arr[1]=1;
+        for(int i=2;i<=n;i++)
+            arr[i]=arr[i-1]+arr[i-2];
+        return arr[n];        
+    }
+    public int fib(int n) {
+        //return recursion(n);
+        return topdown(n);
+       
     }
 }

d9962de6b93a2ee0b7bba36282550348f9a66456
Besita Augustin <besita2000@gmail.com>
2025-08-08 08:43:54 +0530

    Time: 0 ms (100.00%) | Memory: 42.5 MB (17.17%) - LeetSync

--- 17-letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number.java
+++ 17-letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number.java
@@ -5,18 +5,15 @@ class Solution {
             return;
         }
 
-        int index=digits.charAt(idx)-'0';
-        String curr=letters[index];
+        String curr=letters[digits.charAt(idx)-'0'];
         for(int i=0;i<curr.length();i++){
             temp.append(curr.charAt(i));
-            //System.out.println(temp);
             backtrack(idx+1,n,ans,temp,letters,digits);
             temp.deleteCharAt(temp.length()-1);
-            //System.out.println(temp);
         }
     }
     public List<String> letterCombinations(String digits) {
-        String[] letters=new String[10];
+        /*String[] letters=new String[10];
         letters[0]="";
         letters[1]="";
         letters[2]="abc";
@@ -26,14 +23,14 @@ class Solution {
         letters[6]="mno";
         letters[7]="pqrs";
         letters[8]="tuv";
-        letters[9]="wxyz";
+        letters[9]="wxyz";*/
+
+        String[] letters={"","","abc","def","ghi","jkl","mno","pqrs","tuv","wxyz"};
 
         List<String> ans=new ArrayList<>();
         int n=digits.length();
-
         if(n==0)
             return ans;
-
         StringBuilder temp=new StringBuilder();
         backtrack(0,n,ans,temp,letters,digits);     
         return ans;   

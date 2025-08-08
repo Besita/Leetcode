cd4516ee7b599b7c18b633d800dc4cb2d01b1689
Besita Augustin <besita2000@gmail.com>
2025-08-08 23:41:58 +0530

    Time: 0 ms (100.00%) | Memory: 41.1 MB (59.97%) - LeetSync

---
+++ 198-house-robber/house-robber.java
@@ -0,0 +1,86 @@
+class Solution {
+    public int recursion(int n,int[] nums){
+        if(n==0)
+            return nums[0];
+        if(n<0)
+            return 0;
+
+        int pick=nums[n]+recursion(n-2,nums);
+        int notpick=recursion(n-1,nums);
+        return Math.max(pick,notpick);
+    }
+    public int memoization(int n,int[] nums,int[] dp){
+        if(n==0)
+            return dp[0];
+        if(n<0)
+            return 0;
+
+        if(dp[n]!=-1)
+            return dp[n];
+
+        int pick=nums[n]+memoization(n-2,nums,dp);
+        int notpick=memoization(n-1,nums,dp);
+        return dp[n]=Math.max(pick,notpick);
+    }
+    public int tabulation(int[] nums){
+        int n=nums.length;
+        int[] dp=new int[n+1];
+        for(int i=0;i<=n;i++)
+            dp[i]=-1;
+        dp[0]=nums[0];
+        for(int i=1;i<=n;i++){
+            int pick=nums[i],notpick=0;
+            if((i-2)>=0)
+                pick+=dp[i-2];
+            if((i-1)>=0)
+                notpick=dp[i-1];
+            dp[i]=Math.max(pick,notpick);
+        }
+        return dp[n];
+    }
+    public int revision(int[] nums){
+        int n=nums.length;
+        int maxval=0;
+        if(n==1)
+            return nums[0];
+        if(n==2)
+            return Math.max(nums[0],nums[1]);
+        int[] dp=new int[n];
+        dp[0]=nums[0];
+        dp[1]=Math.max(nums[0],nums[1]);
+        for(int i=2;i<n;i++){
+            int pick=nums[i]+dp[i-2];
+            int notpick=dp[i-1];
+            dp[i]=Math.max(pick,notpick);
+        }
+        return dp[n-1];
+    }
+    public int rob(int[] nums) {
+        int n=nums.length-1;
+        return revision(nums);
+        //return recursion(n,nums);
+        /*//memoization
+        int[] dp=new int[n+1];
+        for(int i=0;i<=n;i++)
+            dp[i]=-1;
+        dp[0]=nums[0];
+        return memoization(n,nums,dp);*/
+
+        //tabulation
+        //return tabulation(nums);
+
+        /*//space optimised
+        int prev1=nums[0];
+        int prev2=0;
+        for(int i=1;i<=n;i++){
+            int pick=nums[i],notpick=0;
+            if((i-2)>=0)
+                pick+=prev2;
+            notpick=prev1;
+            int curr=Math.max(pick,notpick);
+            prev2=prev1;
+            prev1=curr;
+        }
+        return prev1;*/
+    }
+}

0cc7fc46f99e0bb44b83cc9f1bf5e2d261e07c98
Besita Augustin <besita2000@gmail.com>
2025-08-09 11:31:54 +0530

    Time: 10 ms (57.71%) | Memory: 45.6 MB (10.96%) - LeetSync

--- 494-target-sum/target-sum.java
+++ 494-target-sum/target-sum.java
@@ -1,18 +1,25 @@
 class Solution {
-    public int dp(int sum,int idx,int n,int[] nums,int target){
+    public int dp(int sum,int idx,int n,int[] nums,int target,int[][] dp){
         if(idx<0){
             if(sum==target)
                 return 1;
             else
                 return 0;
         }
-        int pick=0,notpick=0;        
-        pick=nums[idx]+dp(sum+nums[idx],idx-1,n,nums,target);
-        notpick=-nums[idx]+dp(sum-nums[idx],idx-1,n,nums,target);
-        return (pick+notpick);
+        if(dp[idx][sum+1000]!=-1)
+            return dp[idx][sum+1000];
+
+        int add=0,subtract=0;        
+        add=nums[idx]+dp(sum+nums[idx],idx-1,n,nums,target,dp);
+        subtract=-nums[idx]+dp(sum-nums[idx],idx-1,n,nums,target,dp);
+        return dp[idx][sum+1000]=add+subtract;
     }
     public int findTargetSumWays(int[] nums, int target) {
         int n=nums.length;
-        return dp(0,n-1,n,nums,target);
+        int[][] dp=new int[n][2*1000+1];
+        for(int i=0;i<n;i++){
+            Arrays.fill(dp[i],-1);
+        }
+        return dp(0,n-1,n,nums,target,dp);
     }
 }

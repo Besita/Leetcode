c118e38beaba562439669dbc28bbb5ba9ed0f841
Besita Augustin <besitaaugustin@gmail.com>
2025-08-24 18:49:33 +0530

    Time: 8 ms (27.54%) | Memory: 44.7 MB (49.72%) - LeetSync

---
+++ 657-robot-return-to-origin/robot-return-to-origin.java
@@ -0,0 +1,19 @@
+class Solution {
+    public boolean judgeCircle(String moves) {
+        int originr=0;
+        int originc=0;
+        for(int i=0;i<moves.length();i++){
+            if(moves.charAt(i)=='L')
+                originc--;
+            else if(moves.charAt(i)=='R')
+                originc++;
+            else if(moves.charAt(i)=='U')
+                originr--;
+            else
+                originr++;
+        }
+        if(originr==0 && originc==0)
+            return true;
+        return false;
+    }
+}

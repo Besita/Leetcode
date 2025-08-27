7eee1ec063897df492cc6834f3b46efa70bda400
Besita Augustin <besitaaugustin@gmail.com>
2025-08-28 00:05:46 +0530

    Time: 3 ms (99.67%) | Memory: 45.2 MB (76.13%) - LeetSync

---
+++ 207-course-schedule/course-schedule.java
@@ -0,0 +1,36 @@
+class Solution {
+    public boolean dfs(int node,int n,List<Integer>[] adj,int[] vis,int[] pathvis){
+        if(pathvis[node]==1)
+            return false;
+        
+        if(vis[node]==1)
+            return true;
+        
+        vis[node]=1;
+        pathvis[node]=1;
+        for(int i:adj[node]){
+            if(!dfs(i,n,adj,vis,pathvis))
+                return false;
+        }
+        pathvis[node]=0;
+        return true;
+    }
+    public boolean canFinish(int numCourses, int[][] edges) {
+        List<Integer>[] adj=new ArrayList[numCourses];
+        for(int i=0;i<numCourses;i++)
+            adj[i]=new ArrayList<>();
+
+        for(int[] i:edges)
+            adj[i[0]].add(i[1]);
+
+        int[] vis=new int[numCourses];
+        int[] pathvis=new int[numCourses];
+        for(int i=0;i<numCourses;i++){
+            if(vis[i]==0){ 
+                if(!dfs(i,numCourses,adj,vis,pathvis))
+                    return false;
+            }
+        }        
+        return true;
+    }
+}

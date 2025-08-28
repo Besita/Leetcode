10701a2de15e9494020e9750270adc714608f782
Besita Augustin <besitaaugustin@gmail.com>
2025-08-28 17:27:34 +0530

    Time: 5 ms (70.41%) | Memory: 55.5 MB (75.50%) - LeetSync

--- 820-find-eventual-safe-states/find-eventual-safe-states.java
+++ 820-find-eventual-safe-states/find-eventual-safe-states.java
@@ -1,38 +1,41 @@
 class Solution {
-    public boolean dfs(int node,int V,int[] vis,int[] pathvis,int[][] graph,int[] safe){
-        if(pathvis[node]==1){ 
+    public boolean dfs(int node,int V,int[] vis,int[] pathvis,int[][] graph){
+        if(pathvis[node]==1){  //same path i.e cycle is present
             return true;
         }
-        if(vis[node]==1)
+        if(vis[node]==1) //already visited in another path, not current path i.e no  cycle
             return false;
 
         vis[node]=1;
         pathvis[node]=1;
         for(int i:graph[node]){
-            if(dfs(i,V,vis,pathvis,graph,safe))
+            if(dfs(i,V,vis,pathvis,graph)) //if cycle return , else make pathvis==0
                 return true;
         }
         pathvis[node]=0;
-        safe[node]=1;
         return false;
     }
-    public List<Integer> eventualSafeNodes(int[][] graph) {
+
+    public List<Integer> usingDfsDAG(int[][] graph) {
         int V=graph.length;
         int[] vis=new int[V];
         int[] pathvis=new int[V];
-        int[] safe=new int[V];
         List<Integer> ans=new ArrayList<>();
         
         for(int i=0;i<V;i++){
             if(vis[i]==0){
-                dfs(i,V,vis,pathvis,graph,safe);
+                dfs(i,V,vis,pathvis,graph);
             }
         }
         for(int i=0;i<V;i++){
-            //if(safe[i]==1)
             if(vis[i]==1 && pathvis[i]==0)
                 ans.add(i);
         }
         return ans;
+    }
+    public List<Integer> eventualSafeNodes(int[][] graph) {
+        
+        return usingDfsDAG(graph);
+        
     }
 }

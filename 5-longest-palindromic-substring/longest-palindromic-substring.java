033e3153c3ebbded7887215097ace5b884ad522e
Besita Augustin <besitaaugustin@gmail.com>
2025-09-03 22:02:20 +0530

    Time: 18 ms (56.05%) | Memory: 45.4 MB (32.01%) - LeetSync

--- 5-longest-palindromic-substring/longest-palindromic-substring.java
+++ 5-longest-palindromic-substring/longest-palindromic-substring.java
@@ -1,6 +1,6 @@
 class Solution {
     
-    public String maxpalindromelength(String s,int mid1, int mid2, int n){
+    /*public String maxpalindromelength(String s,int mid1, int mid2, int n){
         int left=mid1;
         int right=mid2;
         while(left>=0 && right<n){
@@ -13,7 +13,6 @@ class Solution {
         return s.substring(left+1,right);
     }
     public String longestPalindrome(String s) {
-
         int n=s.length();
         String ans="";
         for(int i=1;i<n;i++){
@@ -27,6 +26,42 @@ class Solution {
         if(ans.length()==0)
             ans=s.substring(0,1);
         return ans;
+    }*/
+
+
+
+
+
+
+
+
+
+
+
+    public String maxpalin(String s,int left,int right){
+        int n=s.length();
+        while(left>=0 && right<n && s.charAt(left)==s.charAt(right)){
+            left--;
+            right++;
+        }
+        return s.substring(left+1,right);
     }
-    
+
+    public String longestPalindrome(String s) {
+        int n=s.length();
+        String ans="";
+        int maxlen=0;
+        if(n<=1)
+            return s;
+        for(int i=0;i<n-1;i++){
+            String odd=maxpalin(s,i,i);
+            if(odd.length()>ans.length())
+                ans=odd;
+            
+            String even=maxpalin(s,i,i+1);
+            if(even.length()>ans.length())
+                ans=even;
+        }
+        return ans;
+    }
 }

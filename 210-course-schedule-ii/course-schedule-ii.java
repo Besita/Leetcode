758858a26db09259c8651e18514d62b8f5c1adc7
Besita Augustin <besitaaugustin@gmail.com>
2025-09-23 22:40:21 +0530

    Time: 3 ms (99.34%) | Memory: 46.2 MB (31.02%) - LeetSync

--- 210-course-schedule-ii/course-schedule-ii.java
+++ 210-course-schedule-ii/course-schedule-ii.java
@@ -1,7 +1,5 @@
 class Solution {
- 
-    public int[] findOrder(int numCourses, int[][] prerequisites) {
-        
+    public int[] usingBFSTopoSort(int numCourses, int[][] prerequisites){
         int[] indegree=new int[numCourses];
         List<Integer>[] adj=new ArrayList[numCourses];
         for(int i=0;i<numCourses;i++)
@@ -35,5 +33,51 @@ class Solution {
         if(res.length!=numCourses)
             return new int[0];
         return res;
+    }
+    public boolean dfs(int node,int n,List<Integer>[] adj,int[] vis,int[] pathvis,Stack<Integer> st){
+        if(pathvis[node]==1)
+            return false;
+        
+        if(vis[node]==1)
+            return true;
+        
+        vis[node]=1;
+        pathvis[node]=1;
+        for(int i:adj[node]){
+            if(!dfs(i,n,adj,vis,pathvis,st))
+                return false;
+        }
+        pathvis[node]=0;
+        st.push(node);
+        return true;
+    }
+    public int[] usingDFS(int numCourses, int[][] prerequisites){
+        List<Integer>[] adj=new ArrayList[numCourses];
+        for(int i=0;i<numCourses;i++)
+            adj[i]=new ArrayList<>();
+
+        for(int[] i:prerequisites)
+            adj[i[1]].add(i[0]);
+
+        int[] vis=new int[numCourses];
+        int[] pathvis=new int[numCourses];
+        Stack<Integer> st=new Stack<>();
+        for(int i=0;i<numCourses;i++){
+            if(vis[i]==0){ 
+                if(!dfs(i,numCourses,adj,vis,pathvis,st))
+                    return new int[0];
+            }
+        }       
+        int[] ans=new int[numCourses];
+        int i=0;
+        while(!st.empty()){
+            ans[i++]=st.peek();
+            st.pop();
+        }
+        return ans;
+    }
+    public int[] findOrder(int numCourses, int[][] prerequisites) {
+        //return usingBFSTopoSort(numCourses,prerequisites);
+        return usingDFS(numCourses,prerequisites);
     }
 }

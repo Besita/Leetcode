0087625df6542d5e0f230bce412fb1abac462f9b
Besita Augustin <besitaaugustin@gmail.com>
2025-09-26 18:27:39 +0530

    Time: 0 ms (100.00%) | Memory: 42.2 MB (59.90%) - LeetSync

--- 990-verifying-an-alien-dictionary/verifying-an-alien-dictionary.java
+++ 990-verifying-an-alien-dictionary/verifying-an-alien-dictionary.java
@@ -1,9 +1,8 @@
 class Solution {
-    public boolean isAlienSorted(String[] words, String order) {
+    public boolean usingMap(String[] words,String order){
         Map<Character,Integer> map=new HashMap<>();
-        for(int i=0;i<order.length();i++){
+        for(int i=0;i<order.length();i++)
             map.put(order.charAt(i),i);
-        }
 
         for(int i=0;i<words.length-1;i++){
             String s1=words[i];
@@ -24,5 +23,34 @@ class Solution {
                 return false;
         }
         return true;
+    }
+    public boolean usingArray(String[] words, String order) {
+        int[] dict=new int[26];
+        for(int i=0;i<order.length();i++){
+            dict[order.charAt(i)-'a']=i;
+        }
+
+        for(int i=0;i<words.length-1;i++){
+            String s1=words[i];
+            int l1=s1.length();
+            String s2=words[i+1];
+            int l2=s2.length();
+            int len=Math.min(l1,l2);
+            int k=0;
+            while(k<len && s1.charAt(k)==s2.charAt(k))
+                k++;
+            if(k<len){
+                if(dict[s1.charAt(k)-'a']>dict[s2.charAt(k)-'a'])
+                    return false;
+            }
+            else if(l1>l2)
+                return false;
+        }
+        return true;
+    }
+    public boolean isAlienSorted(String[] words, String order) {
+        //return usingMap(words,order);
+        return usingArray(words,order);
+        
     }
 }

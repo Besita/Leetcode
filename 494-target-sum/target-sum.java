da196be1965d42a32b1398aa7d8b06c85cc82fdc
Besita Augustin <besita2000@gmail.com>
2025-11-11 10:24:58 +0530

    Time: 681 ms (5.03%) | Memory: 47 MB (5.22%) - LeetSync

--- 494-target-sum/target-sum.java
+++ 494-target-sum/target-sum.java
@@ -1,6 +1,18 @@
 class Solution {
-    public int dp(int sum,int idx,int n,int[] nums,int target,int[][] dp){
-        return 0;
+    public int dp(int[] nums,int target,int n,int curr,int[][] dp){
+        if(n<0 && curr==target)
+            return 1;
+        else if(n<0)   
+            return 0;
+
+        if(dp[n][curr]!=-1)
+            return dp[n][curr];
+
+        int positive=recursion(nums,target,n-1,curr+nums[n]);        
+        int negative=recursion(nums,target,n-1,curr-nums[n]);
+
+        return dp[n][curr]=positive+negative;
+        
     }
     int recursion(int[] nums,int target,int n,int curr){
         if(n<0 && curr==target)
@@ -8,18 +20,19 @@ class Solution {
         else if(n<0)   
             return 0;
 
-        int pick=recursion(nums,target,n-1,curr+nums[n]);        
-        int notpick=recursion(nums,target,n-1,curr-nums[n]);
+        int positive=recursion(nums,target,n-1,curr+nums[n]);        
+        int negative=recursion(nums,target,n-1,curr-nums[n]);
 
-        return pick+notpick;
+        return positive+negative;
     }
     public int findTargetSumWays(int[] nums, int target) {
         int n=nums.length;
-        return recursion(nums,target,n-1,0);
-        /*int[][] dp=new int[n][2001];
+        //return recursion(nums,target,n-1,0);
+
+        int[][] dp=new int[n][2001];
         for(int i=0;i<n;i++){
             Arrays.fill(dp[i],-1);
         }
-        return dp(0,n-1,n,nums,target,dp); */
+        return dp(nums,target,n-1,0,dp); 
     }
 }

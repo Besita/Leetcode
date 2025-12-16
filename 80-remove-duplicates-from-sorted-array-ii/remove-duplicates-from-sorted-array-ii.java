4913ac84ad62526cfde6e7b7c9523996bf3d8851
Besita Augustin <besita2000@gmail.com>
2025-12-16 22:20:57 +0530

    Time: 0 ms (100.00%) | Memory: 48.9 MB (15.40%) - LeetSync

--- 80-remove-duplicates-from-sorted-array-ii/remove-duplicates-from-sorted-array-ii.java
+++ 80-remove-duplicates-from-sorted-array-ii/remove-duplicates-from-sorted-array-ii.java
@@ -4,22 +4,13 @@ class Solution {
         int prev=0;
         if(n<=2)
             return n;
-        int start=1;
-        if(nums[prev]==nums[prev+1]){ 
-            prev++;
-            start++;
-        }
-        for(int curr=start;curr<n;curr++){
+        
+        for(int curr=2;curr<n;curr++){
             if(nums[prev]!=nums[curr]){
-                nums[prev+1]=nums[curr];
+                nums[prev+2]=nums[curr];
                 prev++;
-                if(curr+1<n && nums[curr]==nums[curr+1]){ 
-                    nums[prev+1]=nums[curr];
-                    prev++;
-                }
-            }
-            
+            }           
         }
-        return prev+1;
+        return prev+2;
     }
 }

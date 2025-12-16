0a75688afea9ff7dba55d7f03ed815e606b1787d
Besita Augustin <besita2000@gmail.com>
2025-12-16 21:15:57 +0530

    Time: 1 ms (69.85%) | Memory: 46.8 MB (11.37%) - LeetSync

--- 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.java
+++ 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.java
@@ -4,18 +4,15 @@ class Solution {
         int prev=0;
         int curr=1;
         while(curr<n){
-            while(curr<n && nums[curr]==nums[prev])
+            /*while(curr<n && nums[curr]==nums[prev])
                 curr++;
             if(curr>=n)
-                break;
+                break;*/
             if(nums[prev]<nums[curr]){
-                int temp=nums[prev+1];
                 nums[prev+1]=nums[curr];
-                nums[curr]=temp;
                 prev=prev+1;
             }
-            else
-                curr++;
+            curr++;
         }
         return prev+1;
     }

5ea42dee25c0fdd8f737bf155fa28ab67314e1d6
Besita Augustin <besita2000@gmail.com>
2025-12-17 17:15:01 +0530

    Time: 0 ms (100.00%) | Memory: 45.7 MB (39.29%) - LeetSync

---
+++ 122-best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int maxProfit(int[] prices) {
+        int n=prices.length;
+        int Profit=0;
+        int currMin=prices[0];
+        for(int i=0;i<n;i++){
+            if(prices[i]>currMin){ 
+                Profit+=prices[i]-currMin;
+                currMin=prices[i];
+            }
+            else
+                currMin=prices[i];
+            
+        }
+        return Profit;
+    }
+}

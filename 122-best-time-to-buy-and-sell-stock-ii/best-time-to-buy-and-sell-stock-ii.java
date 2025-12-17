a913f7114a1dea12596bb57eb9a3af59bf18441f
Besita Augustin <besita2000@gmail.com>
2025-12-17 17:29:53 +0530

    Time: 0 ms (100.00%) | Memory: 45.4 MB (65.60%) - LeetSync

--- 122-best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.java
+++ 122-best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.java
@@ -3,12 +3,12 @@ class Solution {
         int n=prices.length;
         int Profit=0;
         int currMin=prices[0];
-        for(int i=0;i<n;i++){
-            if(prices[i]>currMin)
-                Profit+=prices[i]-currMin;
-                
-            currMin=prices[i];
-            
+        //if the numbers are in ascending order the previous solution works, max-min
+        //here we should sell as soon as the price increases if the rices are random to get max profit
+
+        for(int i=1;i<n;i++){
+            if(prices[i]>prices[i-1])
+                Profit+=prices[i]-prices[i-1];
         }
         return Profit;
     }

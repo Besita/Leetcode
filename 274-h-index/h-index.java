e837dcfe8eb1dea5a690f30f67271c37c0c8b74f
Besita Augustin <besita2000@gmail.com>
2025-12-18 18:36:00 +0530

    Time: 5 ms (52.85%) | Memory: 43.8 MB (7.17%) - LeetSync

--- 274-h-index/h-index.java
+++ 274-h-index/h-index.java
@@ -1,13 +1,15 @@
 class Solution {
     public int hIndex(int[] citations) {
         //since max value of h, sort the citations in decending order
+        //sorting desc order,needs to convert int to wrapper object, to avoid it sort in ascending 
+        //and iterate the index from n-1 to 0. 
         int n=citations.length;
         Arrays.sort(citations);
         int idx;
         int hidx=0;
         for(int i=n-1;i>=0;i--){      
-            idx=n-1-i;      
-            if(citations[i]>idx)
+            idx=n-i;      
+            if(citations[i]>=idx) 
                 hidx++;
         }
         return hidx;

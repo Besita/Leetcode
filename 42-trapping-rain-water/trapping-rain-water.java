f52b9d6d805f16e054d8da645f8ec072b48630e4
Besita Augustin <besita2000@gmail.com>
2025-12-19 14:23:29 +0530

    Time: 1 ms (58.41%) | Memory: 49.2 MB (5.27%) - LeetSync

--- 42-trapping-rain-water/trapping-rain-water.java
+++ 42-trapping-rain-water/trapping-rain-water.java
@@ -1,6 +1,29 @@
 class Solution {
-    public int trap(int[] height) {
-        int n=height.length;
+    public int bruteforce(int []height){
+         int n=height.length;
+        int start=0;
+        int end=n-1;
+        int watertrapped=0;
+        int[] trapped_left=new int[n];
+        int[] trapped_right=new int[n];
+        int max_left=0;
+        int max_right=0;
+        for(int i=0;i<n;i++){
+            max_left=Math.max(max_left,height[i]);
+            trapped_left[i]=max_left-height[i];
+        }
+        for(int i=n-1;i>=0;i--){
+            max_right=Math.max(max_right,height[i]);
+            trapped_right[i]=max_right-height[i];
+        }
+        for(int i=0;i<n;i++){
+            watertrapped+=Math.min(trapped_left[i],trapped_right[i]);
+        }
+        
+        return watertrapped; 
+    }
+    public int optimised(int []height){
+         int n=height.length;
         int start=0;
         int end=n-1;
         int watertrapped=0;
@@ -18,6 +41,10 @@ class Solution {
                 end--;
             }
         }
-        return watertrapped;        
+        return watertrapped; 
+    }
+    public int trap(int[] height) {
+        return bruteforce(height);
+        //return optimised(height);
     }
 }

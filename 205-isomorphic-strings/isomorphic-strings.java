00fefa7b5ca41a626f03be2867482fcfc03d7f5f
Besita Augustin <besita2000@gmail.com>
2026-02-22 06:07:54 +0530

    Time: 12 ms (53.85%) | Memory: 44 MB (41.16%) - LeetSync

--- 205-isomorphic-strings/isomorphic-strings.java
+++ 205-isomorphic-strings/isomorphic-strings.java
@@ -16,14 +16,16 @@ class Solution {
     }
    
     public boolean usingMethod2(String s,String t){
-        int[] s1=new int[256];
-        int[] t1=new int[256];
+        int[] s1=new int[256];// 256 , if it has to hold A-Z,a-z,numbers,special char 
+        int[] t1=new int[256]; //if only A-Z or a-z use 26. ans [ch-'a' or ch-'A']
         for(int i=0;i<s.length();i++){
             char ch1=s.charAt(i);
             char ch2=t.charAt(i);
-            if(s1[ch1]!=t1[ch2])
+            //System.out.println(ch1);
+            //System.out.println(ch2);
+            if(s1[ch1]!=t1[ch2]) //s1[ch1-'a'] does not work if it has to hold A-Z,a-z,numbers,special char 
                 return false;
-            s1[ch1]=i+1;
+            s1[ch1]=i+1;//bcause int[] is initialised to 0 by default, so replace idx 0 as 1
             t1[ch2]=i+1;
         }
         return true;
@@ -33,8 +35,8 @@ class Solution {
         if(s.length()!=t.length())
             return false;
 
-        //return usingMethod1(s,t);
-        return usingMethod2(s,t); // SC:O(n1+n2) //fastest
+        return usingMethod1(s,t);
+        //return usingMethod2(s,t); // SC:O(n1+n2) //fastest
         
     }
 }

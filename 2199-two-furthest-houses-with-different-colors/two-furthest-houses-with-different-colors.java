f5a2460ff9fb902ef00ac716994490773317c36e
Besita Augustin <besita2000@gmail.com>
2026-04-21 19:59:56 +0530

    Time: 0 ms (100.00%) | Memory: 43.5 MB (11.19%) - LeetSync

--- 2199-two-furthest-houses-with-different-colors/two-furthest-houses-with-different-colors.java
+++ 2199-two-furthest-houses-with-different-colors/two-furthest-houses-with-different-colors.java
@@ -1,5 +1,5 @@
 class Solution {
-    public int maxDistance(int[] colors) {
+    public int bruteForce(int[] colors){
         int n=colors.length;
         int maxdist=Integer.MIN_VALUE;
         for(int i=0;i<n;i++){
@@ -9,5 +9,43 @@ class Solution {
             }
         }
         return maxdist;
+    }
+    public int twopointer(int[] colors){
+        int n=colors.length;
+        int maxdist=Integer.MIN_VALUE;
+        int start=0;
+        int end=n-1;
+        while(end<n){
+            if(colors[start]!=colors[end]){
+                maxdist=Math.max(maxdist,end-start);
+                return maxdist;
+            }
+            else
+                start++;
+        }
+        return maxdist;
+    }
+    public int optimised(int[] colors){
+        int n=colors.length;
+        int maxdist=Integer.MIN_VALUE;
+        for(int i=0;i<n;i++){
+            if(colors[i]!=colors[n-1]){ 
+                maxdist=Math.max(maxdist,n-1-i);
+                break;
+            }
+        }
+        for(int i=n-1;i>=0;i--){
+            if(colors[i]!=colors[0]){ 
+                maxdist=Math.max(maxdist,i);
+                break;
+            }
+        }
+        return maxdist;
+    }
+    public int maxDistance(int[] colors) {
+        //return bruteForce(colors);
+        //return twopointer(colors); //don't work for testcase [6,6,6,6,6,6,6,6,6,19,19,6,6]
+        //we need to calculate distancce from both sides instead of two pointer
+        return optimised(colors);
     }
 }

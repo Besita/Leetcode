0a10e80ade02efc82022b2938c0c117299a982bc
Besita Augustin <besita2000@gmail.com>
2026-04-29 01:27:48 +0530

    Time: 34 ms (75.03%) | Memory: 87.9 MB (81.97%) - LeetSync

--- 164-maximum-gap/maximum-gap.java
+++ 164-maximum-gap/maximum-gap.java
@@ -1,5 +1,50 @@
 class Solution {
-    public int maximumGap(int[] nums) {
+    public int RadixSort(int[] nums) { 
+        int n=nums.length;
+        if(n==1)
+            return 0;
+        else if(n==2)
+            return Math.abs(nums[0]-nums[1]);
+        
+        //find max element to find the no. of digits
+        int maxVal=Integer.MIN_VALUE;
+        for(int i=0;i<n;i++){
+            maxVal=Math.max(maxVal,nums[i]);
+        }
+
+        int exp=1; //1,10,100
+        int[] output=new int[n];
+    
+        while(maxVal/exp>0){
+
+            int[] bucket=new int[10];//should be inside loop to reset for each pass
+
+            for(int i=0;i<n;i++){
+                int val=(nums[i]/exp)%10;
+                bucket[val]++;
+            }
+
+            for(int i=1;i<10;i++)
+                bucket[i]+=bucket[i-1];
+
+            for(int i=n-1;i>=0;i--){
+                int val=(nums[i]/exp)%10;
+                output[bucket[val]-1]=nums[i];
+                bucket[val]--;
+            }
+
+            //nums=Arrays.copyOf(output,n);
+            System.arraycopy(output,0,nums,0,n);
+            exp=exp*10;
+        }
+
+        int maxdiff=0;
+        for(int i=1;i<n;i++){
+            maxdiff=Math.max(maxdiff,output[i]-output[i-1]);
+        }
+        return maxdiff;
+    }
+    public int BucketSort(int[] nums) { 
         int n=nums.length;
         if(n==1)
             return 0;
@@ -34,5 +79,10 @@ class Solution {
             prev=maxbucket[i];
         }
         return maxgap;
+    }
+    public int maximumGap(int[] nums) {
+        //return BucketSort(nums);//interviewer favorite.
+        return RadixSort(nums);//easy sort 
+        
     }
 }

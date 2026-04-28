ccc93c3c58d5fc34bb05f0b2e9876b9c6ad7b23d
Besita Augustin <besita2000@gmail.com>
2026-04-28 19:17:28 +0530

    Time: 17 ms (49.77%) | Memory: 46.7 MB (32.76%) - LeetSync

--- 5-longest-palindromic-substring/longest-palindromic-substring.java
+++ 5-longest-palindromic-substring/longest-palindromic-substring.java
@@ -1,43 +1,4 @@
-class Solution {
-    
-    /*public String maxpalindromelength(String s,int mid1, int mid2, int n){
-        int left=mid1;
-        int right=mid2;
-        while(left>=0 && right<n){
-            if(s.charAt(left)==s.charAt(right)){ 
-                left--;right++;
-            }
-            else
-                break;
-        }
-        return s.substring(left+1,right);
-    }
-    public String longestPalindrome(String s) {
-        int n=s.length();
-        String ans="";
-        for(int i=1;i<n;i++){
-            String palinodd=maxpalindromelength(s,i,i,n);
-            if(palinodd.length()>ans.length())
-                ans=palinodd;
-            String palineven=maxpalindromelength(s,i-1,i,n);
-            if(palineven.length()>ans.length())
-                ans=palineven;
-        }
-        if(ans.length()==0)
-            ans=s.substring(0,1);
-        return ans;
-    }*/
-
-
-
-
-
-
-
-
-
-
-
+class Solution {   
     public String maxpalin(String s,int left,int right){
         int n=s.length();
         while(left>=0 && right<n && s.charAt(left)==s.charAt(right)){
@@ -51,9 +12,9 @@ class Solution {
         int n=s.length();
         String ans="";
         int maxlen=0;
-        if(n<=1)
+        if(n==1)
             return s;
-        for(int i=0;i<n-1;i++){
+        for(int i=0;i<n;i++){
             String odd=maxpalin(s,i,i);
             if(odd.length()>ans.length())
                 ans=odd;

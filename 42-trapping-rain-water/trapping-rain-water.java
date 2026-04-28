7b0264b35788c99c5368103f2e5738816476075f
Besita Augustin <besita2000@gmail.com>
2026-04-28 17:29:07 +0530

    Time: 0 ms (100.00%) | Memory: 47.9 MB (46.58%) - LeetSync

--- 42-trapping-rain-water/trapping-rain-water.java
+++ 42-trapping-rain-water/trapping-rain-water.java
@@ -29,7 +29,7 @@ class Solution {
         int watertrapped=0;
         int max_left=0;
         int max_right=0;
-        while(start<end){
+        while(start<=end){
             if(height[start]<=height[end]){
                 max_left=Math.max(max_left,height[start]);
                 watertrapped+=max_left-height[start];

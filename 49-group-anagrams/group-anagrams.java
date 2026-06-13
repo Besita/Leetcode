95c8cc24f4410495396128645b62cf85e0fcfd98
Besita Augustin <besita2000@gmail.com>
2026-06-13 10:27:28 +0530

    Time: 528 ms (5.02%) | Memory: 49.5 MB (83.86%) - LeetSync

---
+++ 49-group-anagrams/group-anagrams.java
@@ -0,0 +1,43 @@
+class Solution {
+    public boolean checkAnagram(String s,String t){
+        if(s.length()!=t.length())
+            return false;
+
+        int[] arr=new int[26];           
+        for(int i=0;i<t.length();i++){ 
+            arr[t.charAt(i)-'a']--;
+            arr[s.charAt(i)-'a']++;
+        }
+        
+        for(int i=0;i<26;i++){
+            if(arr[i]!=0)
+                return false;
+        }
+        return true;
+    }
+
+    public List<List<String>> groupAnagrams(String[] strs) {
+        int n=strs.length;
+        int[] str=new int[n];
+        List<List<String>> ans=new ArrayList<>();
+        
+        for(int i=0;i<n;i++){
+            List<String> curr=new ArrayList<>();
+            if(str[i]==1)
+                continue;
+            str[i]=1;
+            curr.add(strs[i]);
+            for(int j=i+1;j<n;j++){
+                if(str[j]==1)
+                    continue;
+                
+                if(checkAnagram(strs[i],strs[j])){ 
+                    str[j]=1;
+                    curr.add(strs[j]);
+                }
+            }
+            ans.add(curr);
+        }
+        return ans;
+    }
+}

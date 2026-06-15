4f0be2cf22ab9ffc568fa260e1c8470669a2dea2
Besita Augustin <besita2000@gmail.com>
2026-06-16 00:38:57 +0530

    Time: 12 ms (84.13%) | Memory: 47.6 MB (64.94%) - LeetSync

--- 347-top-k-frequent-elements/top-k-frequent-elements.java
+++ 347-top-k-frequent-elements/top-k-frequent-elements.java
@@ -1,5 +1,5 @@
 class Solution {
-    public int[] topKFrequent(int[] nums, int k) {
+    public int[] usingHashmap(int[] nums, int k) {
         int n=nums.length;
         HashMap<Integer,Integer> map=new HashMap<>();
         for(int i=0;i<n;i++)
@@ -18,5 +18,35 @@ class Solution {
                 break;
         }
         return ans;
+    }
+    public int[] usingBucketsort(int[] nums, int k) {
+        int n=nums.length;
+        HashMap<Integer,Integer> map=new HashMap<>();
+        for(int i=0;i<n;i++)
+            map.put(nums[i],map.getOrDefault(nums[i],0)+1);
+
+        List<Integer>[] bucket=new ArrayList[n+1];
+        for(Map.Entry<Integer,Integer> iter : map.entrySet()){
+            if(bucket[iter.getValue()] == null)
+                bucket[iter.getValue()]=new ArrayList<>();
+            bucket[iter.getValue()].add(iter.getKey());
+        }
+
+        int[] ans=new int[k];
+        for(int i=n,count=0;i>0 && count<k;i--){
+            if(bucket[i]!=null){
+                List<Integer> curr=bucket[i];
+                for(int val:curr)
+                    ans[count++]=val;
+            }
+        }
+        return ans;
+    }
+
+    public int[] topKFrequent(int[] nums, int k) {
+        //return usingHashmap(nums,k);
+
+        return usingBucketsort(nums,k);
+        
     }
 }

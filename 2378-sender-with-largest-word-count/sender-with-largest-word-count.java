3eb17bf1099da15c6acaef01b620ce7705f56266
Besita Augustin <besita2000@gmail.com>
2026-06-16 22:41:32 +0530

    Time: 51 ms (74.86%) | Memory: 57.9 MB (41.90%) - LeetSync

--- 2378-sender-with-largest-word-count/sender-with-largest-word-count.java
+++ 2378-sender-with-largest-word-count/sender-with-largest-word-count.java
@@ -7,15 +7,31 @@ class Solution {
             String[] word=messages[i].split(" ");
             map.put(senders[i],map.getOrDefault(senders[i],0)+word.length);
         }
-
+       
+        /*//TC: O(n log n)
         List<Map.Entry<String,Integer>> list=new ArrayList<>(map.entrySet());
-        
+
         Collections.sort(list,(a,b)-> { 
             if(b.getValue()==a.getValue())
                 return b.getKey().compareTo(a.getKey());
             return b.getValue()-a.getValue();
         });
+        
+        return list.get(0).getKey();*/
 
-        return list.get(0).getKey();
+        int largeWord=0;
+        String largeWordName="";
+        for(Map.Entry<String,Integer> iter:map.entrySet()){
+            if(iter.getValue()>largeWord){
+                largeWordName=iter.getKey();
+                largeWord=iter.getValue();
+            }
+            else if(iter.getValue()==largeWord){
+                if(iter.getKey().compareTo(largeWordName)>0)
+                    largeWordName=iter.getKey();
+            }
+        }
+
+        return largeWordName;
     }
 }

532687cd39006c2b66349104ad6d2041c600777c
Besita Augustin <besita2000@gmail.com>
2026-06-28 23:22:08 +0530

    Time: 1 ms (100.00%) | Memory: 50.9 MB (50.59%) - LeetSync

--- 966-binary-subarrays-with-sum/binary-subarrays-with-sum.java
+++ 966-binary-subarrays-with-sum/binary-subarrays-with-sum.java
@@ -13,13 +13,37 @@ class Solution {
                 sum-=nums[left];
                 left++;
             }
-            if(sum<=goal)
-                ans+=right-left+1;
+            //if(sum<=goal)
+            ans+=right-left+1;
+        }
+        return ans;
+    }
+    public int threepointer(int[] nums,int goal){
+        int left=0,mid=0;
+        int sum=0;
+        int ans=0;
+        int n=nums.length;
+        for(int right=0;right<nums.length;right++){
+            sum+=nums[right];
+
+            while(sum>goal){
+                sum-=nums[left];
+                left++;
+                mid=left;
+            }
+
+            if(sum==goal){
+                while(mid<right && sum-nums[mid]==goal){
+                    mid++;
+                }
+                ans+=mid-left+1;
+            }
         }
         return ans;
     }
     public int numSubarraysWithSum(int[] nums, int goal) {
         
-        return slidingwindow(nums,goal)-slidingwindow(nums,goal-1);
+        return slidingwindow(nums,goal)-slidingwindow(nums,goal-1);//better use 2 pointer always for subarray
+        //return threepointer(nums,goal);
     }
 }

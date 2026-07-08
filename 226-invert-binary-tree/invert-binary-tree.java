dbf3011e11c09e287cc68928afd7652d9636e110
Besita Augustin <besita2000@gmail.com>
2026-07-08 23:01:32 +0530

    Time: 0 ms (100.00%) | Memory: 42.8 MB (64.93%) - LeetSync

--- 226-invert-binary-tree/invert-binary-tree.java
+++ 226-invert-binary-tree/invert-binary-tree.java
@@ -14,7 +14,7 @@
  * }
  */
 class Solution {
-    TreeNode recursion(TreeNode root){
+    /*TreeNode recursion(TreeNode root){
         if(root==null)
             return root;
 
@@ -24,6 +24,19 @@ class Solution {
 
         TreeNode left=recursion(root.left);
         TreeNode right=recursion(root.right);      
+        return root;
+    }*/
+    TreeNode recursion(TreeNode root){
+        if(root==null)
+            return root;
+
+        TreeNode left=recursion(root.left);
+        TreeNode right=recursion(root.right);  
+
+        TreeNode temp=root.left;
+        root.left=root.right;
+        root.right=temp;
+            
         return root;
     }
     public TreeNode invertTree(TreeNode root) {        
